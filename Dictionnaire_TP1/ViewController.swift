760e20426c7c57ea00fbcbdab7b37d6e3d574adb
eleves <eleves@rodrigo.local>
2017-11-04 10:44:41 -0400

    Initial Commit

---
+++ Dictionnaire_TP1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dictionnaire_TP1
+//
+//  Created by eleves on 17-11-04.
+//  Copyright © 2017 eleves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
